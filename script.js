7d299ddedc054f418329b91df0709df5f62c09eb
Bhavana Gupta <gupta.bhavana6@gmail.com>
2024-07-28 17:37:45 +0530

    added privacy policy feature

--- script.js
+++ script.js
@@ -23,3 +23,40 @@ if (container && text) {
     breathAnimation();
     setInterval(breathAnimation, totalTime);
 }
+
+// Get the modal
+var modal = document.getElementById("privacyModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("startBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementById("closeModal");
+
+// Get the OK button element
+var okBtn = document.getElementById("modalOkBtn");
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal and go to breathing page
+span.onclick = function() {
+    modal.style.display = "none";
+    window.location.href = "breathing.html";
+}
+
+// When the user clicks the OK button, close the modal and proceed
+okBtn.onclick = function() {
+    modal.style.display = "none";
+    window.location.href = "breathing.html";
+}
+
+// When the user clicks anywhere outside of the modal, close it and go to breathing page
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        window.location.href = "breathing.html";
+    }
+}
